1f91f261964663b00be39ce869da4746515ab0b1
DEVASHISH PANDEY <116454781+devashishpandey90@users.noreply.github.com>
2024-10-22 23:13:52 +0530

    Add files via upload

---
+++ apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "b0332d5086580a479dd1c435fd77d207",
+  base: "https://home.openweathermap.org/api_keys",
+};
